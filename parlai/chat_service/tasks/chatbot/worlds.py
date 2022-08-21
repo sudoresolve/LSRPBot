079dceaeeaaed0a47506247597bdd58ebf403654
sudoresolve <110743973+sudoresolve@users.noreply.github.com>
2022-08-21 02:27:03 -0700

    Update worlds.py
    Updated MODEL_KEY to: bb3_3B

--- parlai/chat_service/tasks/chatbot/worlds.py
+++ parlai/chat_service/tasks/chatbot/worlds.py
@@ -31,7 +31,7 @@ class MessengerBotChatTaskWorld(World):
     """
 
     MAX_AGENTS = 1
-    MODEL_KEY = 'blender_90M'
+    MODEL_KEY = 'bb3_3B'
 
     def __init__(self, opt, agent, bot):
         self.agent = agent
